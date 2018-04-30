2ab48d19beb980fd50fb3cabb727400d45f0506b
K5008133 <eric.noam@mindcurv.com>
2018-04-30 15:25:40 +0200

    Increase timeout for SSE

--- src/main/scala/es/richweb/rest/mocks/StreamServer.scala
+++ src/main/scala/es/richweb/rest/mocks/StreamServer.scala
@@ -1,25 +1,30 @@
 package es.richweb.rest.mocks
 
 import cats.effect.IO
-import fs2.StreamApp.ExitCode
+
 import fs2.{Stream, StreamApp, async}
+import fs2.StreamApp.ExitCode
+
 import org.http4s.ServerSentEvent
 import org.http4s.server.blaze._
 
+import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Properties.envOrNone
 
 object StreamServer extends StreamApp[IO] {
   val port: Int = envOrNone("HTTP_PORT").fold(8000)(_.toInt)
 
-  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
+  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] = {
     Stream.eval(async.circularBuffer[IO, ServerSentEvent](100)).flatMap { q =>
       BlazeBuilder[IO]
         .bindHttp(port)
+        .withIdleTimeout(1.hour)
         .mountService(StaticService.service, "/")
         .mountService(Ping.service(q), "/stream")
         .mountService(Ping.service(q), "/ping")
         .mountService(DateStream.service, "/now")
         .serve
     }
+  }
 }
